bf45bb4968d615b27eb144f68d9be0478be96829
Lidii <lidiiany92@gmail.com>
2019-09-12 15:24:29 -0700

    Beta version of rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,10 +1,34 @@
-#!/usr/bin/python
+
 
 import sys
 
-def rock_paper_scissors(n):
-  pass 
+rps_array = ['rock', 'papper', 'scissors']
 
+#combination_length is 1, 2, 3, etc
+
+def rock_paper_scissors(combination_length):
+
+  printAllCombinations(rps_array, "", combination_length)
+  
+def printAllCombinations(rps_array, prefix, combination_length):
+  
+  print ("\n NEW ITERATION")
+      
+  if (combination_length == 0) : 
+      print('\n Combination of length N \n', prefix) 
+      return
+      
+  for i in range(3): 
+    
+  # Next character of input added 
+    newPrefix = prefix + rps_array[i] + " "
+    print(f'\n prefix {prefix} newPrefix {newPrefix}')
+        
+    # combination_length is decreased, because  
+    # we have added a new character 
+    printAllCombinations(rps_array, newPrefix, combination_length - 1) 
+    
+print(rock_paper_scissors(2))
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
