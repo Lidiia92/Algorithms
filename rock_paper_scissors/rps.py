0c8eb426990aec8550851a68c067c557f0a793d7
Lidii <lidiiany92@gmail.com>
2019-09-12 15:44:34 -0700

    returning array

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,6 +3,7 @@
 import sys
 
 rps_array = ['rock', 'papper', 'scissors']
+result_array = []
 
 #combination_length is 1, 2, 3, etc
 
@@ -15,17 +16,21 @@ def printAllCombinations(rps_array, prefix, combination_length):
   print ("\n NEW ITERATION")
       
   if (combination_length == 0) : 
-      print('\n Combination of length N \n', prefix) 
-      return
+      result_array.append(prefix.split(','))
+
+      for i in range(len(result_array)):
+        for j in range(len(result_array[i])):
+          if result_array[i][j] == '':
+            result_array[i].remove('')
+
+      print(result_array)
+      return result_array 
       
   for i in range(3): 
     
   # Next character of input added 
-    newPrefix = prefix + rps_array[i] + " "
-    print(f'\n prefix {prefix} newPrefix {newPrefix}')
-        
-    # combination_length is decreased, because  
-    # we have added a new character 
+    newPrefix = prefix + rps_array[i] + ","
+
     printAllCombinations(rps_array, newPrefix, combination_length - 1) 
     
 print(rock_paper_scissors(2))
